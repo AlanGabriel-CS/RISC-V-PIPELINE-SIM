3be7066de9ca369964b3f651a33bf6ec37570c0c
AlanGabriel-CS <alankgabriel2007@gmail.com>
2026-08-13 16:27:23 -0400

    Clean up port formatting in instruction_mem.sv

--- rtl/instruction_mem.sv
+++ rtl/instruction_mem.sv
@@ -1,20 +1,18 @@
-module instruction_mem(
-    input logic [31:0] addr,        // 32-bit address from program counter
-    output logic [31:0] instruction // 32-bit machine code instruction out to the decoder 
+module instruction_mem (
+    input  logic [31:0] addr,       // 32-bit address from program counter
+    output logic [31:0] instruction // 32-bit machine code instruction out to the decoder
 );
 
-    // creates physical storage: memory array of 1024 slots, each 32-bits (4 bytes)
-    // gives 4kb instruction memory space
+    // physical storage: 1024 32-bit (4-byte) words -- 4 KB of instruction memory
     logic [31:0] mem [1023:0];
 
     initial begin
     //    $readmemh("instructions.hex", mem);
     end
 
-    //asynch read logic
-    // byte-addressed, pc increments by 4
-    // (0, 4, 8, 12) to array index (0, 1, 2, 3)
-    // divide address by 4, dropping to bottom 2 bits
+    // Asynchronous read logic. Byte-addressed, pc increments by 4, so
+    // (0, 4, 8, 12) maps to array index (0, 1, 2, 3): divide the address by
+    // 4 by dropping the bottom 2 bits.
     assign instruction = mem[addr[11:2]];
-    
+
 endmodule
