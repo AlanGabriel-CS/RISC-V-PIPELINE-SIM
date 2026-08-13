e0416665ba22b8340a2f3cf003a7aeaa17a41fca
AlanGabriel-CS <alankgabriel2007@gmail.com>
2026-08-13 16:27:19 -0400

    Clean up trailing whitespace in forward_unit.sv

--- rtl/forward_unit.sv
+++ rtl/forward_unit.sv
@@ -22,7 +22,7 @@ module forward_unit (
             forward_a = 2'b01;
         else
             forward_a = 2'b00;
- 
+
         if (ex_mem_reg_write && (ex_mem_rd != 5'd0) && (ex_mem_rd == id_ex_rs2))
             forward_b = 2'b10;
         else if (mem_wb_reg_write && (mem_wb_rd != 5'd0) && (mem_wb_rd == id_ex_rs2))
@@ -31,4 +31,3 @@ module forward_unit (
             forward_b = 2'b00;
     end
 endmodule
- 
